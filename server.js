dfaf85e5435a1269e2ca8a97a9e2f0a8f90f03c6
noreikam <noreikam1@gmail.com>
2022-04-12 23:10:59 -0400

    established connection to express server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello world'
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
